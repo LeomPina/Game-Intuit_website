debe82d58af4bce5dbb02e0a48cc878c036077d1
Leompina <leonardopina2001@gmail.com>
2025-02-13 17:22:53 +0000

    homepage chinese translation added

---
+++ assets/js/lang.js
@@ -0,0 +1,19 @@
+{
+  "en": {
+    "nav_about": "About Us",
+    "nav_tech": "Tech",
+    "nav_news": "News",
+    "nav_contact": "Contact Us",
+    "hero_title": "Game-Intuit",
+    "hero_description": "Unlock the Secret Behind Winning"
+  },
+  "zh": {
+    "nav_about": "关于我们",
+    "nav_tech": "技术",
+    "nav_news": "新闻",
+    "nav_contact": "联系我们",
+    "hero_title": "Game-Intuit",
+    "hero_description": "赢得比赛的秘密"
+  }
+}
+
